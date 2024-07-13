31cc298922540b6f44e26916ee2f5b8762e1eff7
mikeyxb <mr.m.a.brennan@gmail.com>
2024-07-13 12:12:45 +0100

    Paging update and some CSS changes

--- web/src/components/inventory/InventoryGrid.tsx
+++ web/src/components/inventory/InventoryGrid.tsx
@@ -9,8 +9,6 @@ import { createPortal } from 'react-dom';
 import InventoryContext from './InventoryContext';
 
 
-const PAGE_SIZE = 30;
-
 const InventoryGrid: React.FC<{ inventory: Inventory, direction: 'left' | 'right'}> = ({ inventory, direction  }) => {
   const weight = React.useMemo(
     () => (inventory.maxWeight !== undefined ? Math.floor(getTotalWeight(inventory.items) * 1000) / 1000 : 0),
@@ -41,18 +39,16 @@ const InventoryGrid: React.FC<{ inventory: Inventory, direction: 'left' | 'right
           </div>
           <WeightBar percent={inventory.maxWeight ? (weight / inventory.maxWeight) * 100 : 0} />
         </div>
-        <div className={direction === 'left' ? 'inventory-grid-container' : 'inventory-grid-container-right'}>
+        <div className={direction === 'left' ? 'inventory-grid-container' : 'inventory-grid-container-right'} ref={containerRef}>
           <>
-          {inventory.items.slice(0, (page + 1) * PAGE_SIZE).map((item, index) => {
-  if(index < 5 && inventory.type==='player') {
-    return '';
-  }
-
+          {inventory.items.map((item, index) => {
+                if(index < 5 && inventory.type==='player') {
+                  return ''
+                }
   return (
     <InventorySlot
       key={`${inventory.type}-${inventory.id}-${item.slot}`}
       item={item}
-      ref={index === (page + 1) * PAGE_SIZE - 1 ? ref : null}
       inventoryType={inventory.type}
       inventoryGroups={inventory.groups}
       inventoryId={inventory.id}
